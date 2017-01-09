70b0b803ac43d4b0f4677e63db3fefe85d6797c9
Yong Lei <will19ie95@gmail.com>
2017-01-09 16:33:50 -0500

    .

---
+++ canvas.js
@@ -0,0 +1,71 @@
+var gridSize = 16;
+
+
+$(document).ready(function() {
+	create_Canvas();
+	draw();
+})
+
+function create_Canvas() {
+
+	for (var i = 0; i < gridSize * gridSize; i++) {
+		$('#canvas').append('<div class="box outline"></div>')
+	}
+
+	$('.box').css({
+		'height' : $('#canvas').height() / gridSize,
+		'width' : $('#canvas').width() / gridSize,
+	});
+
+	$('#currentSetting').replaceWith('<p id="currentSetting">Current Setting: ' + gridSize + "<p>");
+
+}
+
+function recreate_canvas() {
+	gridSize = $('#grid_Modal #new_gridSize').val().trim();
+	$('#canvas').html('');
+	create_Canvas();
+	draw();
+}
+
+function draw() {
+
+	$('.box').hover(function() {
+		$(this).css('background-color', '#999');
+	})
+
+}
+
+function draw_color() {
+	var randomColor = getRandomColor();
+
+	$('.box').hover(function() {
+		$(this).css('background-color', randomColor);
+		randomColor = getRandomColor();
+	})
+}
+
+function erase() {
+
+	$('.box').hover(function() {
+		$(this).css('background-color','white');
+	})
+
+}
+
+function clear_canvas() {
+	$('.box').css('background-color','white');
+}
+
+function getRandomColor () {
+  var rancomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  return rancomColor;
+}
+
+function outLine() {
+	if($('.box').hasClass('outline')) {
+		$('.box').removeClass('outline');
+	} else {
+		$('.box').addClass('outline');
+	}
+}
